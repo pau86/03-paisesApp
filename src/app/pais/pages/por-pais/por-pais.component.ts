02c51b9a25b2529a2aa504335aef368917b81252
Paulina Pérez <paulinabp8601@gmail.com>
2021-12-07 20:06:18 +0100

    Video 107 | Typing the API Request for RestCountries

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -16,8 +16,8 @@ export class PorPaisComponent {
     console.log(this.termino);
 
     this.paisService.buscarPais(this.termino).subscribe(
-      (resp) => {
-        console.log(resp);
+      (paises) => {
+        console.log(paises);
       },
       (err) => {
         this.hayError = true;
