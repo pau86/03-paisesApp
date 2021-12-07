028479edebd11a8e7983fc5886983cdefc291ed0
Paulina Pérez <paulinabp8601@gmail.com>
2021-12-07 19:53:14 +0100

    Video 106 | Error handling - in API requests

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -4,18 +4,24 @@ import { PaisService } from '../../services/pais.service';
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
-  styles: [],
 })
 export class PorPaisComponent {
   termino: string = '';
+  hayError: boolean = false;
 
   constructor(private paisService: PaisService) {}
 
   buscar() {
+    this.hayError = false;
     console.log(this.termino);
 
-    this.paisService.buscarPais(this.termino).subscribe((resp) => {
-      console.log(resp);
-    });
+    this.paisService.buscarPais(this.termino).subscribe(
+      (resp) => {
+        console.log(resp);
+      },
+      (err) => {
+        this.hayError = true;
+      }
+    );
   }
 }
